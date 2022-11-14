50133453cbd8b0775e1697c38102542aa5c64253
Ira Filonova <74858792+wonder-filka@users.noreply.github.com>
2022-11-14 22:05:27 +0200

    fixed

---
+++ README.md
@@ -0,0 +1,27 @@
+# Form send to e-mail.
+
+The task was to create the HTML form that sends me an email.
+Buy the domain and host it.
+
+## Screenshot
+
+![](./Screenshot.png)
+
+### Links
+
+- GitHub URL: [GitHub](https://github.com/wonder-filka/)
+- Live Site URL: [lolly-oops-shop](http://lolly-oops-shop.website/)
+
+## My process
+
+### Built with
+
+- HTML5
+- CSS3
+- PHPmailer
+- ES6 - JS
+
+## Author
+
+- Website - [Iryna Filonova](https://sensational-cactus-93a152.netlify.app/)
+- Twitter - [@filochka2](https://twitter.com/filochka2)
--- js/script.js
+++ js/script.js
@@ -1,109 +1,44 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("form");
+  let form = document.getElementById("contact");
+  let userNumber = document.querySelector("#formPhone");
+  let labelPhone = document.getElementById("labelPhone");
+  let error = false;
+  userNumber.addEventListener("input", formValid);
   form.addEventListener("submit", formSend);
+
+  function formValid(input) {
+    let numerValue = input.target.value;
+    if (isNaN(numerValue)) {
+      labelPhone.innerHTML = "Wrong format";
+      userNumber.className = "inputWar form_input";
+      error = false;
+    } else {
+      labelPhone.innerHTML = "";
+      userNumber.className = "form_input";
+      error = true;
+    }
+  }
 
   async function formSend(e) {
     e.preventDefault();
-
-    let error = formValidate(form);
-
-    let formData = new FormData(form);
-    formData.append("image", formImage.files[0]);
-
-    if (error === 0) {
-      form.classList.add("_sending");
-      let response = await fetch("index.php", {
+    if (error !== false) {
+      let formData = new FormData(form);
+      console.log(formData);
+      let response = await fetch("sendmail.php", {
         method: "POST",
         body: formData,
       });
       if (response.ok) {
         let result = await response.json();
         alert(result.message);
-        formPreview.innerHTML = "";
         form.reset();
-        form.classList.remove("_sending");
       } else {
         alert("Ошибка");
-        form.classList.remove("_sending");
       }
     } else {
-      alert("Заполните обязательные поля");
+      return null;
     }
-  }
-
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelectorAll("._req");
-
-    for (let index = 0; index < formReq.length; index++) {
-      const input = formReq[index];
-      formRemoveError(input);
-
-      if (input.classList.contains("_email")) {
-        if (emailTest(input)) {
-          formAddError(input);
-          error++;
-        }
-      } else if (
-        input.getAttribute("type") === "checkbox" &&
-        input.checked === false
-      ) {
-        formAddError(input);
-        error++;
-      } else {
-        if (input.value === "") {
-          formAddError(input);
-          error++;
-        }
-      }
-    }
-    return error;
-  }
-  function formAddError(input) {
-    input.parentElement.classList.add("_error");
-    input.classList.add("_error");
-  }
-  function formRemoveError(input) {
-    input.parentElement.classList.remove("_error");
-    input.classList.remove("_error");
-  }
-  //Функция теста email
-  function emailTest(input) {
-    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-  }
-
-  //Получаем инпут file в переменную
-  const formImage = document.getElementById("formImage");
-  //Получаем див для превью в переменную
-  const formPreview = document.getElementById("formPreview");
-
-  //Слушаем изменения в инпуте file
-  formImage.addEventListener("change", () => {
-    uploadFile(formImage.files[0]);
-  });
-
-  function uploadFile(file) {
-    // провераяем тип файла
-    if (!["image/jpeg", "image/png", "image/gif"].includes(file.type)) {
-      alert("Разрешены только изображения.");
-      formImage.value = "";
-      return;
-    }
-    // проверим размер файла (<2 Мб)
-    if (file.size > 2 * 1024 * 1024) {
-      alert("Файл должен быть менее 2 МБ.");
-      return;
-    }
-
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
-    };
-    reader.onerror = function (e) {
-      alert("Ошибка");
-    };
-    reader.readAsDataURL(file);
   }
 });
